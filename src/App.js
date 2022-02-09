457b8ac95114943ef68719350e790553061113cc
Saul Calleja <scalleja12345@gmail.com>
2022-02-08 20:48:18 -0500

    Made a textbox that changes colors depending on users input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("black");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          backgroundColor: color,
+          width: "100px",
+          height: "100px",
+          padding: "40px",
+          margin: "auto",
+        }}
+      ></div>
+      <input value={color} onChange={(e) => setColor(e.target.value)} />
     </div>
   );
 }
